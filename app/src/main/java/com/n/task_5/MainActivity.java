85aecb6f290ad34eaf1fe8b42db17a778318c20d
Anton Lo <antshot19@gmail.com>
2019-08-09 12:14:53 +0300

    Task5

--- app/src/main/java/com/n/task_5/MainActivity.java
+++ app/src/main/java/com/n/task_5/MainActivity.java
@@ -9,10 +9,13 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.d(TAG, "onCreate: ");
     }
 
 
