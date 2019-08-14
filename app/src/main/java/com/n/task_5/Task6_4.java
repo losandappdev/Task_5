7f36d2cb9befc354c32e1250a0d4c2a87d22cfcb
Anton Lo <antshot19@gmail.com>
2019-08-14 23:36:10 +0300

    Task6.

---
+++ app/src/main/java/com/n/task_5/Task6_4.java
@@ -0,0 +1,14 @@
+package com.n.task_5;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Task6_4 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_task6_4);
+    }
+}
